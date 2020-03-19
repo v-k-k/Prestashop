3f408db1d00b16dcba49ffa5877e5fe42ccd821a
v-k-k <vkryshtop@web100-krg.local>
2020-03-19 16:46:01 +0200

    before new branch

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,4 +1,5 @@
 from settings import FIREFOX, CHROME, URL
+from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
 from selenium import webdriver
 import pytest
 
@@ -8,14 +9,15 @@ BROWSER = None
 
 def pytest_addoption(parser):
     parser.addoption(
-        "--browser_name", action="store", default="firefox"
+        "--browser_name", action="store", default="chrome"
     )
 
 
 @pytest.fixture(scope="class", autouse=True)
 def setup(request):
-    global BROWSER
+    global BROWSER, desiredCapabilities
     browser_name = request.config.getoption("browser_name")
+
     if browser_name == "chrome":
         BROWSER = webdriver.Chrome(executable_path=CHROME)
     elif browser_name == "firefox":
@@ -27,3 +29,4 @@ def setup(request):
     request.cls.browser = BROWSER
     yield
     BROWSER.close()
+
